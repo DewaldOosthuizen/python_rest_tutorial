714784a85e6b3ba68d50e8fd6fdf9d4c310c209d
Dewald Oosthuizen <dewald.oosthuizen1@gmail.com>
2021-06-03 16:21:34 +0200

    Default debuuger to false

--- web/app.py
+++ web/app.py
@@ -185,4 +185,5 @@ api.add_resource(Save, '/save')
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=5000)
+    # app.run(host='0.0.0.0', debug=True)
